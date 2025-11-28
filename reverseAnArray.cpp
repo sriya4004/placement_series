7df40ad0b4506c7b99236eb9b18c66d98b5db3da
Sriya Pandey <sriyapandey0405@gmail.com>
2025-11-28 17:18:47 +0530

    Create reverseAnArray.cpp

---
+++ reverseAnArray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        int n = arr.size();
+        int s = 0; int e = n-1;
+        while( s<e ){
+            swap(arr[s++],arr[e--]);
+        }
+        
+    }
+};
